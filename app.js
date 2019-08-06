61c76e0c20fed333cba57f78ca0480f62af8a905
Kyawzin995 <Kyawzin995>
2019-08-06 09:39:16 +0630

    created

---
+++ app.js
@@ -0,0 +1,33 @@
+new Vue({
+   el:'#app',
+   data:{
+     
+ },
+   methods:{
+    playSound1(sound) {
+         var audio = new Audio(sound);
+          audio.play();
+        
+      },
+   
+    playSound2(sound){
+        var audio = new Audio(sound);
+        audio.play();
+       },
+
+    playSound3(sound){
+        var audio = new Audio(sound);
+        audio.play();
+       },
+    playSound4(sound){
+        var audio = new Audio(sound);
+        audio.play();
+       },
+    playSound5(sound){
+        var audio = new Audio(sound);
+        audio.play();
+       },
+             
+    }
+  
+});
